d523a76d99c12ef8f4a75be1abd1b48641143b39
nicho10 <knight.johnathan@gmail.com>
2015-03-29 18:33:26 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,20 @@
+#! /bin/bash
+#firts script
+# new to linux, there for got tired of hanging around
+# installing what I wanted on my machine
+#author, Johnathan Knight
+
+
+#update the packages
 apt-get -y update
+#upgrade installed packages 
 apt-get -y upgrade
+#install custom packages
 apt-get install -y htop nmap tmux wireshark vim vlc 
+#update packages
 apt-get -y update
+#update installed packages
+apt-get -y upgrade
 echo " 
 +---------------------------------------------+
 | init application level installation complete|

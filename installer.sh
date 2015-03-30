4ee211da12d13322437ea088e4e89c767f1ac770
nicho10 <knight.johnathan@gmail.com>
2015-03-29 18:25:28 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,5 +2,11 @@ apt-get -y update
 apt-get -y upgrade
 apt-get install -y htop nmap tmux wireshark vim vlc 
 apt-get -y update
-echo "done"
+echo " 
++---------------------------------------------+
+| init application level installation complete|
++---------------------------------------------+
+                     Will Reboot
+"
+
 reboot
